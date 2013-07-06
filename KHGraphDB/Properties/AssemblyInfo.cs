5136f7ffcbe1928ff855376ac7cd1d918dd39efe
zfaustk <4340287+zfaustk@users.noreply.github.com>
2013-07-06 12:44:12 +0000

    Add GraphReader and GraphWriter
    KHG1 is a binary snapshot of vertices, types and edges. Layout,
    color and the rest of the view stay out of the file. Demo does
    not keep the graph in RAM after the window closes.

--- KHGraphDB/Properties/AssemblyInfo.cs
+++ KHGraphDB/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6a5fb37c-0502-444c-bdf7-c5c6954da70d")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
