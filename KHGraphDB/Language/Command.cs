c8b8d8450fdcf150f52ba422661ced8979125a6a
zfaustk <4340287+zfaustk@users.noreply.github.com>
2013-09-14 10:48:02 +0000

    Add find, near and type commands
    A small command language on top of the graph. find walks a type,
    near is BFS SearchNearby, type lists the vertices of a named type.
    No parser generator. Split, lookup, return.

---
+++ KHGraphDB/Language/Command.cs
@@ -0,0 +1,209 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using KHGraphDB.Algorithm;
+using KHGraphDB.Structure.Interface;
+
+namespace KHGraphDB.Language
+{
+    /// <summary>
+    /// 2013 command language: find / near / type.
+    /// </summary>
+    public class Command
+    {
+        readonly IGraph _graph;
+        readonly BFS _bfs = new BFS();
+
+        public Command(IGraph graph)
+        {
+            if (graph == null)
+                throw new ArgumentNullException("graph");
+            _graph = graph;
+        }
+
+        public CommandResult Run(string text)
+        {
+            if (text == null)
+                return CommandResult.Fail("empty");
+            string s = text.Trim();
+            if (s.Length == 0)
+                return CommandResult.Fail("empty");
+
+            string[] parts = Split(s);
+            if (parts.Length == 0)
+                return CommandResult.Fail("empty");
+
+            string head = parts[0].ToLowerInvariant();
+            if (head == "find")
+                return Find(parts);
+            if (head == "near")
+                return Near(parts);
+            if (head == "type")
+                return TypeOf(parts);
+            return CommandResult.Fail("2013 language: find Person | find Person name=Alice | near Alice 2 | type Idea");
+        }
+
+        CommandResult Find(string[] parts)
+        {
+            if (parts.Length < 2)
+                return CommandResult.Fail("find Type [key=value]");
+            string typeName = parts[1];
+            string key = null;
+            string val = null;
+            if (parts.Length >= 3)
+                ParseEq(parts[2], out key, out val);
+
+            IType t = _graph.GetTypeByName(typeName);
+            List<IVertex> hits = new List<IVertex>();
+            if (t != null)
+            {
+                foreach (IVertex v in t.Vertices)
+                {
+                    if (key == null || EqualsStr(v[key], val))
+                        hits.Add(v);
+                }
+            }
+            return CommandResult.Ok(hits.Count.ToString() + " vertex", hits);
+        }
+
+        CommandResult Near(string[] parts)
+        {
+            if (parts.Length < 2)
+                return CommandResult.Fail("near Name [hops]");
+            string name = Unquote(parts[1]);
+            int hops = 2;
+            if (parts.Length >= 3)
+                int.TryParse(parts[2], out hops);
+
+            IVertex src = FindByName(name);
+            if (src == null)
+                return CommandResult.Fail("vertex not found");
+            List<IVertex> nearby = _bfs.SearchNearby(_graph, src, hops);
+            List<IVertex> all = new List<IVertex>(nearby.Count + 1);
+            all.Add(src);
+            all.AddRange(nearby);
+            _bfs.EndAlgorithm(_graph);
+            return CommandResult.Ok("near " + name + " hops=" + hops, all);
+        }
+
+        CommandResult TypeOf(string[] parts)
+        {
+            if (parts.Length < 2)
+                return CommandResult.Fail("type Name");
+            IType t = _graph.GetTypeByName(parts[1]);
+            if (t == null)
+                return CommandResult.Fail("type not found");
+            List<IVertex> hits = new List<IVertex>();
+            foreach (IVertex v in t.Vertices)
+                hits.Add(v);
+            return CommandResult.Ok("type " + t.Name, hits);
+        }
+
+        IVertex FindByName(string name)
+        {
+            foreach (IVertex v in _graph.Vertices)
+            {
+                if (EqualsStr(v["name"], name))
+                    return v;
+            }
+            IVertex byId = _graph.GetVertex(name);
+            return byId;
+        }
+
+        static bool EqualsStr(object left, string right)
+        {
+            if (left == null)
+                return right == null;
+            return string.Equals(left.ToString(), right, StringComparison.Ordinal);
+        }
+
+        static void ParseEq(string token, out string key, out string val)
+        {
+            key = null;
+            val = null;
+            int i = token.IndexOf('=');
+            if (i <= 0)
+                return;
+            key = token.Substring(0, i);
+            val = Unquote(token.Substring(i + 1));
+        }
+
+        static string Unquote(string s)
+        {
+            if (s == null || s.Length < 2)
+                return s;
+            char a = s[0];
+            char b = s[s.Length - 1];
+            if ((a == '"' && b == '"') || (a == '\'' && b == '\''))
+                return s.Substring(1, s.Length - 2);
+            return s;
+        }
+
+        static string[] Split(string s)
+        {
+            List<string> parts = new List<string>();
+            StringBuilder cur = new StringBuilder();
+            bool inQuote = false;
+            char q = '\0';
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (inQuote)
+                {
+                    if (c == q)
+                    {
+                        inQuote = false;
+                    }
+                    else
+                    {
+                        cur.Append(c);
+                    }
+                    continue;
+                }
+                if (c == '"' || c == '\'')
+                {
+                    inQuote = true;
+                    q = c;
+                    continue;
+                }
+                if (char.IsWhiteSpace(c))
+                {
+                    if (cur.Length > 0)
+                    {
+                        parts.Add(cur.ToString());
+                        cur.Length = 0;
+                    }
+                    continue;
+                }
+                cur.Append(c);
+            }
+            if (cur.Length > 0)
+                parts.Add(cur.ToString());
+            return parts.ToArray();
+        }
+    }
+
+    public class CommandResult
+    {
+        public bool Succeeded { get; private set; }
+        public string Message { get; private set; }
+        public IList<IVertex> Vertices { get; private set; }
+
+        CommandResult(bool ok, string message, IList<IVertex> vertices)
+        {
+            Succeeded = ok;
+            Message = message;
+            Vertices = vertices ?? new List<IVertex>();
+        }
+
+        public static CommandResult Ok(string message, IList<IVertex> vertices)
+        {
+            return new CommandResult(true, message, vertices);
+        }
+
+        public static CommandResult Fail(string message)
+        {
+            return new CommandResult(false, message, null);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -5,8 +5,10 @@ A Graph Database. Vertex, Edge, Type. By kinghand crew.
 C# 5 / .NET 4.5. No packages. The graph is directed.
 Type is a first-class object, not a string on the vertex.
 
-BFS stores color / dist / pred in AlgorithmObjs and wipes them in
-EndAlgorithm. The walk uses a queue and a visited color so Lookup is a dictionary on KHID. Adding a vertex does not walk the set.
+- KHID identity map, O(1) lookup
+- Type indexed by name
+- BFS scratch in AlgorithmObjs, wiped in EndAlgorithm
+- KHG1 snapshot, the view is not the data
+- Commands: `find Person` · `find Person name=Alice` · `near Alice 2` · `type Idea`
 
-
-
+The original homework lives in zfaustk/-GraphDB and is not modified.
