04ff362ecbce0c37b1667619629402da6aa4ac89
zfaustk <4340287+zfaustk@users.noreply.github.com>
2014-02-02 16:20:11 +0000

    Read KHG2 and keep KHG1
    New files are KHG2. Old homework snapshots still load.
    A vertex that wore two types comes back wearing two types.
    Tests round-trip Ada through a MemoryStream.

---
+++ KHGraphDB.Tests/SnapshotTests.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.IO;
+using KHGraphDB.Helper;
+using KHGraphDB.Structure;
+using KHGraphDB.Structure.Interface;
+
+namespace KHGraphDB.Tests
+{
+    public static class SnapshotTests
+    {
+        public static void Run()
+        {
+            Graph g = new Graph();
+            IType person = g.AddType("Person", null);
+            IType author = g.AddType("Author", null);
+            IType knows = g.AddType("KNOWS", null);
+            Dictionary<string, object> a = new Dictionary<string, object>();
+            a["name"] = "Ada";
+            IVertex ada = g.AddVertex(a, person);
+            ada.AddType(author);
+            Dictionary<string, object> b = new Dictionary<string, object>();
+            b["name"] = "Bob";
+            IVertex bob = g.AddVertex(b, person);
+            g.AddEdge(ada, bob, knows);
+
+            MemoryStream ms = new MemoryStream();
+            GraphWriter.Write(g, ms);
+            ms.Position = 0;
+            Graph h = GraphReader.Read(ms);
+            Assert.Eq(2L, h.VertexCount, "roundtrip vertices");
+            Assert.Eq(1L, h.EdgeCount, "roundtrip edges");
+            IVertex ada2 = h.GetVertexByName("Ada");
+            Assert.NotNull(ada2, "Ada came back");
+            Assert.IsTrue(ada2.HasType("Person"), "Person survived");
+            Assert.IsTrue(ada2.HasType("Author"), "Author survived");
+            int n = 0;
+            foreach (IEdge e in h.GetEdgesByType("KNOWS"))
+                n++;
+            Assert.Eq(1, n, "KNOWS survived");
+        }
+    }
+}
