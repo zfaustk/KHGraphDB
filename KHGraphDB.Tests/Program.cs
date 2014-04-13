bb5e6f72725a3699ae1ee67275d8759cedfb9e02
zfaustk <4340287+zfaustk@users.noreply.github.com>
2014-04-13 16:22:07 +0000

    Test MATCH one-hop both ways
    Outgoing and inbound. The social graph has two KNOWS
    edges. If either direction is wrong the count is wrong.

--- KHGraphDB.Tests/Program.cs
+++ KHGraphDB.Tests/Program.cs
@@ -14,6 +14,7 @@ namespace KHGraphDB.Tests
             IndexTests.Unique();
             QueryTests.Nodes();
             QueryTests.OneHop();
+            QueryTests.Inbound();
             Console.WriteLine("KHGraphDB.Tests");
             Console.WriteLine("passed=" + Assert.Passed + " failed=" + Assert.Failed);
             return Assert.Failed == 0 ? 0 : 1;
