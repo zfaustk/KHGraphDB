deceb217976afb2a017666526c677f068c9d4a96
zfaustk <4340287+zfaustk@users.noreply.github.com>
2014-11-15 14:47:02 +0000

    Cover unknown types in MATCH tests
    Nope is not a Type. The query fails. Empty is the wrong
    answer for a misspelling.

--- KHGraphDB.Tests/Program.cs
+++ KHGraphDB.Tests/Program.cs
@@ -27,6 +27,7 @@ namespace KHGraphDB.Tests
             QueryTests.Optional();
             QueryTests.MergeNode();
             QueryTests.MergeEdge();
+            QueryTests.UnknownType();
             Console.WriteLine("KHGraphDB.Tests");
             Console.WriteLine("passed=" + Assert.Passed + " failed=" + Assert.Failed);
             return Assert.Failed == 0 ? 0 : 1;
