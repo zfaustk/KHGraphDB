dcd8620833a03565e842c8d93613c4dad06ccc21
zfaustk <4340287+zfaustk@users.noreply.github.com>
2019-06-01 09:08:55 +0000

    A sentinel promotes on missed beats
    One watcher. No quorum. The beat file is
    the pulse. Enough unchanged beats and the
    replica is home.

---
+++ src/tests/sentinel.rs
@@ -0,0 +1,48 @@
+//! One watcher. Missed beats promote a replica.
+
+use std::fs;
+use crate::{Sentinel, Store};
+use super::common::attrs;
+
+fn tmp(name: &str) -> std::path::PathBuf {
+    let p = std::env::temp_dir().join(format!("khl1-{}-{}", std::process::id(), name));
+    let _ = fs::remove_dir_all(&p);
+    p
+}
+
+#[test]
+fn missed_beats_promote() {
+    let prim = tmp("p-sen");
+    let copy = tmp("r-sen");
+    let mut s = Store::open(&prim, "notes", 1).unwrap();
+    s.graph_mut().add_vertex(attrs("Ada"), Some("Doc")).unwrap();
+    s.commit().unwrap();
+    let mut r = Store::tail(&copy, &prim, "notes").unwrap();
+    let mut w = Sentinel::new(&prim, 2);
+    assert!(!w.poll(&mut r));
+    assert!(r.is_replica());
+    assert!(w.poll(&mut r));
+    assert!(!r.is_replica());
+    r.graph_mut().add_vertex(attrs("Zed"), Some("Doc")).unwrap();
+    r.commit().unwrap();
+    let _ = fs::remove_dir_all(&prim);
+    let _ = fs::remove_dir_all(&copy);
+}
+
+#[test]
+fn a_beat_resets_miss() {
+    let prim = tmp("p-beat");
+    let copy = tmp("r-beat");
+    let mut s = Store::open(&prim, "notes", 1).unwrap();
+    s.graph_mut().add_vertex(attrs("Ada"), Some("Doc")).unwrap();
+    s.commit().unwrap();
+    let mut r = Store::tail(&copy, &prim, "notes").unwrap();
+    let mut w = Sentinel::new(&prim, 2);
+    assert!(!w.poll(&mut r));
+    s.graph_mut().add_vertex(attrs("Bob"), Some("Doc")).unwrap();
+    s.commit().unwrap();
+    assert!(!w.poll(&mut r));
+    assert!(r.is_replica());
+    let _ = fs::remove_dir_all(&prim);
+    let _ = fs::remove_dir_all(&copy);
+}
